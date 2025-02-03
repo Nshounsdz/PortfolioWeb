91e0f61a412c3265d62fa76b83f6eac23d160efd
Mario <naim.nshou@gmail.com>
2025-02-04 00:12:05 +0100

    galerie2

--- galerie.js
+++ galerie.js
@@ -4,10 +4,12 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('visible');
+        } else {
+            entry.target.classList.remove('visible'); // Enlève la classe lorsqu'il n'est plus visible
         }
     });
 }, {
-    threshold: 0.1
+    threshold: 0.1 // Ajustez le seuil selon vos besoins
 });
 
 fadeElements.forEach(el => observer.observe(el));
